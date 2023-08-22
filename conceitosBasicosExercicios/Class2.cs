101ba27e0ef5bcf66fa8d941f4596137e147c309
Flavio <flavio.novaes29@gmail.com>
2023-08-22 19:46:57 -0300

    Primeiro commit

---
+++ conceitosBasicosExercicios/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conceitosBasicosExercicios
+{
+    internal class Conversor
+    {
+        public double MetrosMilimetros(double metros)
+        {
+            return metros = 1000;
+        }
+    }
+}
---
+++ conceitosBasicosExercicios/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using conceitosBasicosExercicios;
+
+int n1 = 20;
+int n2 = 11, result;
+
+Somador soma;
+soma = new Somador();
+
+Conversor conversor = new Conversor();
+
+result = soma.Soma(n1, n2);
+
+Console.WriteLine(result);
+Console.WriteLine(conversor.MetrosMilimetros(1.76));
