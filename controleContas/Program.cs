3e362b5615168d70837e6df0449317fe57d0b2ed
Flavio <flavio.novaes29@gmail.com>
2023-09-05 20:47:31 -0300

    Primeiro commit

---
+++ controleContas/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using controleContas;
+
+Conta conta1 = new Conta(123456);
+Conta conta2 = new Conta(654321);
+conta2.Saldo = 2341.42m;
+conta1.Saldo = conta2.Saldo * 0.7m;
+
+Console.WriteLine($"Número da Conta: {conta1.Numero} com saldo {conta1.Saldo}");
+Console.WriteLine($"Número da Conta: {conta2.Numero} com saldo {conta2.Saldo}");
