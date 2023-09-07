83995cff20676db9b00de213cc0578eef522ac11
Flavio Novaes <flavio.novaes29@gmail.com>
2023-09-07 10:14:20 -0300

    Adicionado classe Cliente

--- controleContas/Program.cs
+++ controleContas/Program.cs
@@ -2,6 +2,8 @@
 
 using controleContas;
 
+Cliente cliente1 = new Cliente("Flávio", 21, "19794742732");
+
 Conta conta1 = new Conta(123456);
 Conta conta2 = new Conta(654321);
 conta2.Saldo = 2341.42m;
@@ -13,3 +15,5 @@ conta2.AtualizarSaldo(2000m);
 Console.WriteLine($"Número da Conta: {conta1.Numero} com saldo {conta1.Saldo}");
 Console.WriteLine($"Número da Conta: {conta2.Numero} com saldo {conta2.Saldo}");
 Console.WriteLine($"Saldo total geral: {Conta.SaldoTotalGeral}");
+Console.WriteLine($"Dados do cliente: {cliente1.Nome}, {cliente1.Idade}, {cliente1.CPF}");
+
