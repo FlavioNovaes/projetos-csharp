c44d47b1c1c8e6adec7d7293e743f6cd471e1380
Flávio Novaes de Oliveira <109079626+FlavioNovaes@users.noreply.github.com>
2023-10-10 21:40:52 -0300

    Delete SistemaFinanceiroUGB232 directory

--- SistemaFinanceiroUGB232/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaFinanceiroUGB232.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
